a5997ecc531ba487916009dc397d4238e574d0f5
akari39 <xiaofan2.rs@icloud.com>
2025-09-29 16:31:30 +1000

    Upgraded to sdk36.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "app.septs.euiccprobe"
-    compileSdk = 34
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "app.septs.euiccprobe"
         minSdk = 21
-        targetSdk = 34
+        targetSdk = 36
         versionCode = 8
         versionName = "1.0"
 
@@ -45,11 +45,11 @@ dependencies {
     implementation("io.noties.markwon:core:4.6.2")
     implementation("io.noties.markwon:ext-tasklist:4.6.2")
 
-    implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
+    implementation("androidx.core:core-ktx:1.17.0")
+    implementation("androidx.appcompat:appcompat:1.7.1")
+    implementation("com.google.android.material:material:1.13.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.1")
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.2.1")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+    androidTestImplementation("androidx.test.ext:junit:1.3.0")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
 }
